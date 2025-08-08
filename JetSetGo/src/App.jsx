5d504074ef8ec052f4d058206168fdfc04b60f75
Yashsavi Negi <negiyashsavi@gmail.com>
2025-08-07 20:06:18 -0500

    New version of website

---
+++ JetSetGo/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <Navbar /> 
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
