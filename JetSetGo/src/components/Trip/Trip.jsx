0be7dd2cc60520b02c0d6cee4421cb8ab0afe4c9
Yashsavi Negi <negiyashsavi@gmail.com>
2025-08-16 22:07:44 -0500

    Home done

--- JetSetGo/src/components/Trip/Trip.jsx
+++ JetSetGo/src/components/Trip/Trip.jsx
@@ -56,7 +56,7 @@ const Trip = () => {
 
   return (
     <div className="max-w-5xl mx-auto p-6">
-      <h1 className="text-4xl font-bold mb-6">Trip Dashboard</h1>
+      <h1 className="text-4xl font-bold mb-6"></h1>
 
       {/* Tabs */}
       <div className="flex space-x-4 border-b-2 border-gray-200 mb-6">
--- JetSetGo/src/components/navbar/nav.jsx
+++ JetSetGo/src/components/navbar/nav.jsx
@@ -3,15 +3,15 @@ import PlaneLogo from './Screenshot_2025-07-29_235902-removebg-preview.png';
 
 const Navbar = () => {
     return (
-        <nav className="bg-white shadow-md fixed w-full z-10 top-0 left-0">
+        <nav className="shadow-md fixed w-full z-10 top-0 left-0">
             <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
                 <div>
                     <Link to="/" className="flex items-center space-x-3 font-semibold hover:text-blue-600 font-mono">
                         <img src={PlaneLogo} alt="Plane Logo" className="w-10 h-10 object-contain" />
-                        <span>JetSetGo</span>
+                        <span className = "text-white">JetSetGo</span>
                     </Link>
                 </div>
-                <ul className="space-x-8 text-gray-700 flex">
+                <ul className="space-x-8 text-white flex">
                     <li><Link to="/" className="hover:text-blue-600">Home</Link></li>
                     <li><Link to="/dashboard" className="hover:text-blue-600">Dashboard</Link></li>
                 </ul>
